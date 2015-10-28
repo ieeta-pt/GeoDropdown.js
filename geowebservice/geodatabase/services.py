3b18bc8e7b9156c70efdb7c55c7044f16355c288
Luis A. Bastiao Silva <luis.kop@gmail.com>
2015-10-28 16:56:53 +0000

    Add only the necessary data.

--- geowebservice/geodatabase/services.py
+++ geowebservice/geodatabase/services.py
@@ -44,11 +44,10 @@ Search service, that contains all the information about contries.
 It also has methods to load the initial data and to update them 
 """
 class ServiceSolr(object):
-    CONNECTION_TIMEOUT_DEFAULT = 10
+    CONNECTION_TIMEOUT_DEFAULT = 10000000
     
     def __fetch_initial_settings(self):
         try:
-            
             self.SOLR_HOST = settings.SOLR_HOST
             self.SOLR_PORT = settings.SOLR_PORT
             self.SOLR_PATH = settings.SOLR_PATH
@@ -76,8 +75,12 @@ class ServiceSolr(object):
             list_docs_to_commit = []
             i = 0 
             for row in spamreader:
-                i = i + 1
                 
+                if not (row[1]=='Earth' or row[7] == 'CONT' or row[7] == 'PCLI' or row[7] == 'ADM1' or  row[7] == 'ADM2' or  row[7] == 'ADM3' or  row[7] == 'ADM4'): 
+                    continue
+                
+                i = i + 1
+
                 d = {}
                 d['geonameId_t'] = row[0]
                 d['name_t'] = row[1]
@@ -86,25 +89,26 @@ class ServiceSolr(object):
                 d['latitude_f'] = row[4]
                 d['longitude_f'] = row[5]
                 d['fclass_t'] = row[6]
-                d['country_t'] = row[7]
-                d['cc2_t'] = row[8]
-                d['admin1_t'] = row[9]
-                d['admin2_t'] = row[10]
-                d['admin3_t'] = row[11]
-                d['admin4_t'] = row[12]
-                d['population_t'] = row[13]
-                d['elevation_t'] = row[14]
-                d['gtopo30_t'] = row[15]
-                d['timezone_t'] = row[16]
+                d['fcode_t'] = row[7]
+                d['country_t'] = row[8]
+                d['cc2_t'] = row[9]
+                d['admin1_t'] = row[10]
+                d['admin2_t'] = row[11]
+                d['admin3_t'] = row[12]
+                d['admin4_t'] = row[13]
+                d['population_t'] = row[14]
+                d['elevation_t'] = row[15]
+                d['gtopo30_t'] = row[16]
+                d['timezone_t'] = row[17]
                 #print(d)
                 d = dict(d.items())
                 list_docs_to_commit.append(d)
-                if (i%6000==0):
+                if (i%60000==0):
                     xml_answer = self.solr.add(list_docs_to_commit)
                     list_docs_to_commit = []
                     print(i)    
                     
-            xml_answer = self.solr.add([list_docs_to_commit])
+            xml_answer = self.solr.add(list_docs_to_commit)
             list_docs_to_commit = []
             self.solr.optimize()
                 
